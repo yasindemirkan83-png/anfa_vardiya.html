8164606bd5b58145b5eeccac73a934b59eb0937e
yasindemirkan83-png <yasindemirkan83@gmail.com>
2026-03-10 16:11:46 +0300

    Refactor service worker for asset caching

--- Sw.js
+++ Sw.js
@@ -1,10 +1,16 @@
 const CACHE_NAME = 'vardiya-v1';
-const ASSETS = ['./', './index.html', './manifest.json', './bildirim.json'];
+const ASSETS = [
+  './',
+  './index.html',
+  './manifest.json',
+  './vardiya.png'
+];
 
-self.addEventListener('install', e => {
-  e.waitUntil(caches.open(CACHE_NAME).then(c => c.addAll(ASSETS)));
+self.addEventListener('install', (e) => {
+  e.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS)));
 });
 
-self.addEventListener('fetch', e => {
-  e.respondWith(fetch(e.request).catch(() => caches.match(e.request)));
+self.addEventListener('fetch', (e) => {
+  e.respondWith(caches.match(e.request).then(res => res || fetch(e.request)));
 });
+
