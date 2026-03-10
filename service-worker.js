47201aed63c416532d0d8f4b9186ac3f75ccfb2d
yasindemirkan83-png <yasindemirkan83@gmail.com>
2026-03-10 06:44:10 +0300

    Refactor service worker caching logic

--- service-worker.js
+++ service-worker.js
@@ -1,14 +1,21 @@
-const CACHE_NAME = "vardiya-cache-v1";
-const urlsToCache = ["./","./index.html","./vardiya.jpg"];
+const cacheName = 'vardiya-cache-v1';
+const filesToCache = [
+  '/',
+  '/index.html',
+  '/style.css', // Eğer ayrıysa
+  '/manifest.json'
+];
 
-self.addEventListener("install", event => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
+self.addEventListener('install', e=>{
+  e.waitUntil(
+    caches.open(cacheName).then(cache=>cache.addAll(filesToCache))
   );
 });
 
-self.addEventListener("fetch", event => {
-  event.respondWith(
-    caches.match(event.request).then(response => response || fetch(event.request))
+self.addEventListener('fetch', e=>{
+  e.respondWith(
+    caches.match(e.request).then(res=>{
+      return res || fetch(e.request);
+    })
   );
 });
