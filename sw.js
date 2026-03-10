aee87ba3791e6856fc2778911135575057eddec4
yasindemirkan83-png <yasindemirkan83@gmail.com>
2026-03-10 18:09:39 +0300

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,15 @@
+const CACHE_NAME = 'vardiya-v2';
+const ASSETS = [
+  './',
+  './index.html',
+  './manifest.json',
+  './vardiya.png'
+];
+
+self.addEventListener('install', (e) => {
+  e.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS)));
+});
+
+self.addEventListener('fetch', (e) => {
+  e.respondWith(caches.match(e.request).then(res => res || fetch(e.request)));
+});
