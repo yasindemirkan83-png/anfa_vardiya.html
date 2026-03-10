c76ee834c43e24ac3c222205557920f5cadbe7e9
yasindemirkan83-png <yasindemirkan83@gmail.com>
2026-03-10 18:51:19 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'vardiya-v4';
+const CACHE_NAME = 'vardiya-vfinal';
 const ASSETS = [
   './',
   './index.html',
@@ -7,6 +7,7 @@ const ASSETS = [
 ];
 
 self.addEventListener('install', (e) => {
+  self.skipWaiting();
   e.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS)));
 });
 
