ecf7501dc257bc18d39c7226b87e10dcbaec9bcc
yasindemirkan83-png <yasindemirkan83@gmail.com>
2026-03-12 05:13:38 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -6,21 +6,26 @@ const ASSETS = [
   './vardiya.png'
 ];
 
-// Yükleme ve Önbelleğe Alma
 self.addEventListener('install', (e) => {
-  e.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS)));
+  self.skipWaiting();
+  e.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS))
+  );
 });
 
-// İnternet olmasa da çalışma (Offline)
+self.addEventListener('activate', (e) => {
+  e.waitUntil(
+    caches.keys().then(keys => {
+      return Promise.all(keys.map(key => {
+        if (key !== CACHE_NAME) return caches.delete(key);
+      }));
+    })
+  );
+});
+
 self.addEventListener('fetch', (e) => {
-  e.respondWith(caches.match(e.request).then(res => res || fetch(e.request)));
-});
-
-// Bildirime tıklandığında uygulamayı açma
-self.addEventListener('notificationclick', (event) => {
-  event.notification.close();
-  event.waitUntil(
-    clients.openWindow('/')
+  e.respondWith(
+    caches.match(e.request).then(res => res || fetch(e.request))
   );
 });
 
